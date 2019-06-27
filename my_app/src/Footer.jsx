9387c07f6108354f2ec58e52709e011f4a9480de
Guillaume Ribéroux <guillaume.riberoux@gmail.com>
2019-06-27 10:43:11 +0200

    Footer created, no style add

---
+++ my_app/src/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class Navbar extends React.Component {
+  render() {
+    return (
+      <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-dark fixed-bottom"> 
+        <p>
+            Team ELO-Wild || Guilhem Demont, Stéphane Vivier, Fabien Goyhex, Guillaume Ribéroux
+        </p>
+        </nav>
+      </div>
+    );
+  }
+}
