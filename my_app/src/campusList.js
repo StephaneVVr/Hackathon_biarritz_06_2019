ab6096a6b744c6560a75a725dd2cc047e74d59c1
Stéphane Vivier <contact.xsprod@gmail.com>
2019-06-27 12:29:01 +0200

    Install axios, and fetch the API on campuses root

--- my_app/src/campusList.js
+++ my_app/src/campusList.js
@@ -1,49 +1,70 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 //import { Link } from 'react-router-dom';
-import { ListGroup, ListGroupItem, Input } from 'reactstrap';
-import * as campusData from './campuses.json';
+import { ListGroup, ListGroupItem, Input } from "reactstrap";
+import axios from "axios";
+
+// Component for listed Campuses
 
 class CampusList extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			search: ''
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: "",
+      campus: []
+    };
+  }
 
-	updateSearch(event) {
-		this.setState({ search: event.target.value.substr(0, 20) });
-	}
+  // Call API with Axios in /campuses root and edit the campus state with campuses datas
 
-	render() {
-		let filteredcampus = campusData.data.filter(campuseSearch => {
-			return campuseSearch.name.toLowerCase().indexOf(this.state.search.toLowerCase()) !== -1;
-		});
-		const { buttonLabel } = this.props;
-		return (
-			<div className='mt-3'>
-				<h4 style={{ textAlign: 'center' }}>Campus List</h4>
-				<Input
-					type='text'
-					value={this.state.search}
-					onChange={this.updateSearch.bind(this)}
-					className='col-7'
-					placeholder='Recherche'
-				/>
-				<ListGroup className='mt-2'>
-					{filteredcampus.map(campus => (
-						<div>
-							{/* <Link to={`/${campus.name}`} style={{ textDecoration: 'none', color: 'black' }}> */}
-							<ListGroupItem>
-								{buttonLabel} {campus.name}
-							</ListGroupItem>
-							{/* </Link> */}
-						</div>
-					))}
-				</ListGroup>
-			</div>
-		);
-	}
+  componentDidMount() {
+    axios
+      .get(
+        "https://us-central1-rc-league.cloudfunctions.net/wildcodeschool/campuses"
+      )
+      .then(response => {
+        this.setState({
+          campus: response.data
+        });
+      });
+  }
+
+  updateSearch(event) {
+    this.setState({ search: event.target.value.substr(0, 20) });
+  }
+
+  render() {
+    let filteredCampus = this.state.campus.filter(campuseSearch => {
+      return (
+        campuseSearch.name
+          .toLowerCase()
+          .indexOf(this.state.search.toLowerCase()) !== -1
+      );
+    });
+    const { buttonLabel } = this.props;
+    return (
+      <div className="mt-3">
+        <h4 style={{ textAlign: "center" }}>Campus List</h4>
+        <Input
+          type="text"
+          value={this.state.search}
+          onChange={this.updateSearch.bind(this)}
+          className="col-7"
+          placeholder="Recherche"
+        />
+        <ListGroup className="mt-2">
+          {filteredCampus.map(campus => (
+            <div>
+              {/* <Link to={`/${campus.name}`} style={{ textDecoration: 'none', color: 'black' }}> */}
+              <ListGroupItem>
+                {buttonLabel} {campus.name}
+              </ListGroupItem>
+              {/* </Link> */}
+            </div>
+          ))}
+        </ListGroup>
+      </div>
+    );
+  }
 }
 
 export default CampusList;
