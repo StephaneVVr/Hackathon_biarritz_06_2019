6bdde8cd03e1fcda3ccfa08ef81f15c3af46d5b0
Stéphane Vivier <contact.xsprod@gmail.com>
2019-06-27 16:30:25 +0200

    [WIP] Make a routes for data files

--- back/index.js
+++ back/index.js
@@ -1,17 +1,33 @@
+// Call dependencies require, define the port of the server and files require
+
 const express = require("express");
 const app = express();
-const port = 3000;
-const axios = require("axios");
+const port = 8000;
+const campuses = require("./campuses.json");
+const wilders = require("./wilders.json");
+const matches = require("./matches.json");
 
-app.get("/", (req, res) => {
-  connection.query("", (err, results) => {
-    if (err) {
-      res.status(500).send("Erreur lors de la récupération des données");
-    } else {
-      res.json(results);
-    }
-  });
+// Routes GET
+
+// Call campuses.json
+
+app.get("/campuses", (req, res) => {
+  res.status(200).send({ campuses });
 });
+
+// Call wilders.json
+
+app.get("/wilders", (req, res) => {
+  res.status(200).send({ wilders });
+});
+
+// Call matches.json
+
+app.get("/matches", (req, res) => {
+  res.status(200).send({ matches });
+});
+
+// Listening server on port 8000
 
 app.listen(port, err => {
   if (err) {
