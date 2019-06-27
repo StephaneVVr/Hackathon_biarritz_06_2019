027c45fc847ac8d3b86c236ed5d5d0b82549fef2
fabiengoyhex <fabien.goyhex@gmail.com>
2019-06-27 17:34:04 +0200

    added wilder list component and fetch the api

--- my_app/src/Navbar.jsx
+++ my_app/src/Navbar.jsx
@@ -1,57 +1,56 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Navbar extends React.Component {
-  render() {
-    return (
-      <div>
-        <nav class="navbar navbar-expand-lg navbar-light bg-dark">
-          <a class="navbar-brand" href="/">
-            ELO-Wild
-          </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNavDropdown"
-            aria-controls="navbarNavDropdown"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon" />
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul class="navbar-nav">
-              <li class="nav-item active">
-                <a class="nav-link">
-                  <Link to="/">Home</Link>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link">
-                  <Link to="/Leaderboard">Leaderboard</Link>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link">
-                  <Link to="/Matches">Matches History</Link>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link">
-                  <Link to= "/CampusList">Campuses</Link> 
-                </a>
-              </li>
-              <li class="nav-item">
-                <a
-                  class="nav-link">
-                    <Link to="/Wilders">Wilders List</Link>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<nav class='navbar navbar-expand-lg navbar-light bg-dark'>
+					<a class='navbar-brand' href='/'>
+						ELO-Wild
+					</a>
+					<button
+						class='navbar-toggler'
+						type='button'
+						data-toggle='collapse'
+						data-target='#navbarNavDropdown'
+						aria-controls='navbarNavDropdown'
+						aria-expanded='false'
+						aria-label='Toggle navigation'
+					>
+						<span class='navbar-toggler-icon' />
+					</button>
+					<div class='collapse navbar-collapse' id='navbarNavDropdown'>
+						<ul class='navbar-nav'>
+							<li class='nav-item active'>
+								<a class='nav-link'>
+									<Link to='/'>Home</Link>
+								</a>
+							</li>
+							<li class='nav-item'>
+								<a class='nav-link'>
+									<Link to='/Leaderboard'>Leaderboard</Link>
+								</a>
+							</li>
+							<li class='nav-item'>
+								<a class='nav-link'>
+									<Link to='/Matches'>Matches History</Link>
+								</a>
+							</li>
+							<li class='nav-item'>
+								<a class='nav-link'>
+									<Link to='/CampusList'>Campuses</Link>
+								</a>
+							</li>
+							<li class='nav-item'>
+								<a class='nav-link'>
+									<Link to='/WildersList'>Wilders List</Link>
+								</a>
+							</li>
+						</ul>
+					</div>
+				</nav>
+			</div>
+		);
+	}
 }
