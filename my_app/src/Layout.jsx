43b38208ac19e502a1582b5680496677f348b4ed
Guillaume Ribéroux <guillaume.riberoux@gmail.com>
2019-06-27 10:54:24 +0200

    Layout created, no style add

---
+++ my_app/src/Layout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from './Navbar';
+import Footer from './Footer';
+
+export default class Layout extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbar/>
+        <Footer/>
+      </div>
+    );
+  }
+}
