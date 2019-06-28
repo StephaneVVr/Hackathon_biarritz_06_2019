74e54a4dd50c4f5cd6bf7a98fde54dde6212668c
Guillaume Ribéroux <guillaume.riberoux@gmail.com>
2019-06-28 14:03:36 +0200

    last commit

--- my_app/src/Navbar.jsx
+++ my_app/src/Navbar.jsx
@@ -5,8 +5,8 @@ export default class Navbar extends React.Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-          <a className="navbar-brand "style={{color :"white"}} href="/">
+        <nav className="navbar navbar-expand-xl navbar-light bg-dark">
+          <a className="navbar-brand " style={{ color: "white" }} href="/">
             ELO-Wild
           </a>
           <button
@@ -20,21 +20,26 @@ export default class Navbar extends React.Component {
           >
             <span className="navbar-toggler-icon" />
           </button>
-          <div className="collapse navbar-collapse d-flex justify-content-around" id="navbarNavDropdown">
-            <div >
-              <Link to="/Leaderboard"style={{color :"white"}}>Leaderboard</Link>
-            </div>
-
+          <div className="collapse navbar-collapse " id="navbarNavDropdown">
             <div>
-              <Link to="/Matches"style={{color :"white"}}>Matches History</Link>
+              <Link to="/Leaderboard" style={{ color: "white" }}>
+                Leaderboard
+              </Link>
             </div>
-
             <div>
-              <Link to="/CampusList"style={{color :"white"}}>Campuses</Link>
+              <Link to="/Matches" style={{ color: "white" }}>
+                Matches History
+              </Link>
             </div>
-
             <div>
-              <Link to="/WildersList"style={{color :"white"}}>Wilders List</Link>
+              <Link to="/CampusList" style={{ color: "white" }}>
+                Campuses
+              </Link>
+            </div>
+            <div>
+              <Link to="/WildersList" style={{ color: "white" }}>
+                Wilders List
+              </Link>
             </div>
           </div>
         </nav>
